c497e3e4380526cf1c69c3088cd1cbfbf56ade9b
Valentin <vf59165@hotmail.com>
2021-06-10 22:00:54 +0200

    deconnexion_faite

---
+++ DAO.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$port = "3306";
+$dbname = "restaurant";
+$charset = "UTF8";
+$user = "phpmyadmin";
+$password = "root";
+
+$dsn = "mysql:host=$host:$port;dbname=$dbname;charset=$charset";
