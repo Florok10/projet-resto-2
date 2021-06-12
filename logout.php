7f2ddda64eb19b6ebfb948ffb36d8c9847f6574c
Valentin <vf59165@hotmail.com>
2021-06-12 09:58:50 +0200

    toujours_sur_la_deco2

--- logout.php
+++ logout.php
@@ -1,9 +1,31 @@
 <?php
 session_start();
-require_once 'controllerLogin.php';
-require_once 'header.inc.php';
+
+var_dump(session_status());
+
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
 
 
 session_destroy();
 
-header('Location: index.php');
+
+
+// var_dump(session_start());
+
+// session_unset();
+
+// var_dump(session_unset());
+
+// session_destroy();
+
+// var_dump(session_destroy());
+
+ header('Refresh: 100;url=index.php');?>
