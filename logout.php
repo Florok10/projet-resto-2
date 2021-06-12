288ec9d231a56756323ace159391c5f463e637df
Valentin <vf59165@hotmail.com>
2021-06-12 09:28:23 +0200

    toujours_sur_la_deco

--- logout.php
+++ logout.php
@@ -3,13 +3,7 @@ session_start();
 require_once 'controllerLogin.php';
 require_once 'header.inc.php';
 
-if(isset(session_status()) {
-
 
 session_destroy();
 
-echo "Vous êtes déconnecté";
-
 header('Location: index.php');
-}
-endif;
