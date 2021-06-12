d698d9be280869ad7764c5700a53916af8d0841e
Valentin <vf59165@hotmail.com>
2021-06-12 10:59:15 +0200

    toujours_sur_la_deco3

--- controller.php
+++ controller.php
@@ -1,5 +1,9 @@
 <?php 
 
+session_start();//Démarre une nouvelle session ou en reprend une existante
+class Controller {  } //crée une classe controller vide pour le moment
+
+
 require_once "Resto.php";
 require_once "DAO.php";
 
--- logout.php
+++ logout.php
@@ -1,31 +1,12 @@
 <?php
-session_start();
+  require_once(__dir__ . '/controller.php'); //requiert le dossier du fichier et controller.php une seule fois chacun
 
-var_dump(session_status());
+  class Logout extends Controller { //on crée la classe Logout qui s'étend dans la classe Controller
 
-$_SESSION = array();
-
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-
-
-session_destroy();
-
-
-
-// var_dump(session_start());
-
-// session_unset();
-
-// var_dump(session_unset());
-
-// session_destroy();
-
-// var_dump(session_destroy());
-
- header('Refresh: 100;url=index.php');?>
+    public function __construct() // on crée le constructeur de notre classe
+    {
+      session_destroy(); //on utilise la fonction php pour terminet une session
+      header('Refresh: 1;url=index.php'); //on est ensuite redirigé sur index.php
+    }
+  }
+?>
