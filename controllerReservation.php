cf75f455ac7f7f436844e9d653faed522ba8ee1f
Valentin <vf59165@hotmail.com>
2021-06-12 22:55:07 +0200

    reservation3

--- controllerReservation.php
+++ controllerReservation.php
@@ -2,3 +2,7 @@
 
 require_once 'Reservation.php';
 require_once 'Resto.php';
+
+if(isset($_POST['submit_reservation']) && !empty($_POST['date']) && !empty($_POST['choixResto'])){
+    
+}
--- listResto.php
+++ listResto.php
@@ -2,6 +2,7 @@
 session_start();
  $restos = $_SESSION['AllResto'];
  require_once 'controllerResto.php';
+//  require_once __DIR__ . 'controllerReservation.php';
  require_once 'header.inc.php';
 
 ?>
@@ -10,10 +11,20 @@ session_start();
   <div class="album py-5 bg-light">
     <div class="container">
 
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
-      <?php foreach ($restos as $resto ): ?>
+              
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <?php foreach ($restos as $resto ): ?>
         <div class="col-3 col-6">
+        <form class="col-3 col-6" action="controllerReservation.php" method="POST">
+          <select class="form-select form-select-lg mb-3">
+            <option selected value="">Choisissez ce restaurant</option>
+            <option name="choixResto" value="<?= $resto["nameResto"]?>"><?= $resto["nameResto"]?></option>
+          </select>
+          <input type="datetime-local" id="datetime-local" name="date">
+          <button class="btn btn-primary" type="submit" name="submitReservation">Réserver</button>
+        </form>
           <div class="card shadow-sm" style="width: 22rem">
             <img class="bd-placeholder-img card-img-top" src="<?= $resto["pictureResto"]?>" alt="">
             <div class="card-body">
@@ -21,8 +32,6 @@ session_start();
               <p class="card-text"><?= $resto["typeResto"]?></p>
               <p class="card-text"><?= $resto["descriptionResto"]?></p>
               <div class="d-flex justify-content-between align-items-center">
-              <a class="btn btn-primary" href="#">Voir détails</a>
-                <small class="text-muted">9 mins</small>
               </div>
             </div>
           </div>
