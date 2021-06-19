71d8368a045608cef4523a939fdc8785ef96cec0
Valentin <vf59165@hotmail.com>
2021-06-19 19:01:18 +0200

    toujours_sur_le_mode_admin3

--- listResto.php
+++ listResto.php
@@ -23,9 +23,8 @@ session_start();
               <td><?= $resto['typeResto'];?></td>
               <td><img href="<?= $resto['pictureResto'];?>" alt="Image du restaurant"></td>
               <td style="width=200;">
-                <!-- pour que $_GET['id'] puisse récupèrer l'id via url pour la requete de la méthode voirResto -->
-                <!-- <?php echo "<a class='btn btn-info' href='voirResto.php?id='.$resto[id_resto]>Voir</a>";?>
-                <?= "<a class='btn btn-danger' href='reservResto.php?id='.$resto[id_resto]>Résever</a>";?> -->
+                <a class="btn btn-info" href="voirResto.php?id=`.<?php $resto['id_resto']?>`">Voir</a>
+                <a class="btn btn-danger" href="reservResto.php?id=`.<?php $resto['id_resto']?>`">Résever</a>
               </td>
           </tr>
       <?php endforeach; ?>
